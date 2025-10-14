458f97a77a9c5cc16dcef81b8f525d46a5482950
spencrc@debian <145073726+spencrc@users.noreply.github.com>
2025-10-14 11:11:24 -0400

    switched from deprecated write to Writer

--- src/response.zig
+++ src/response.zig
@@ -2,14 +2,29 @@ const std = @import("std");
 const Connection = std.net.Server.Connection;
 
 pub fn send_200(conn: Connection) !void {
-    const message = ("HTTP/1.1 200 OK\nContent-Length: 48" ++
-        "\nContent-Type: text/html\n" ++
-        "Connection: close\n\n<html><body>" ++
-        "<h1>Hello, World!</h1></body></html>");
-    _ = try conn.stream.writeAll(message);
+    const message = ("HTTP/1.1 200 OK\n" ++
+        "Content-Length: 48\n" ++
+        "Content-Type: text/html\n" ++
+        "Connection: close\n" ++
+        "\n" ++
+        "<html><body>" ++
+        "<h1>Hello, World!</h1>" ++
+        "</body></html>");
+    var stream_writer = conn.stream.writer(&.{});
+    const writer = &stream_writer.interface;
+    _ = try writer.write(message);
 }
 
 pub fn send_404(conn: Connection) !void {
-    const message = ("HTTP/1.1 404 Not Found\nContent-Length: 52" ++ "\nContent-Type: text/html\n" ++ "Connection: Closed\n\n<html><body>" ++ "<h1>File not found!</h1></body></html>");
-    _ = try conn.stream.writeAll(message);
+    const message = ("HTTP/1.1 404 Not Found\n" ++
+        "Content-Length: 52\n" ++
+        "Content-Type: text/html\n" ++
+        "Connection: Closed\n" ++
+        "\n" ++
+        "<html><body>" ++
+        "<h1>File not found!</h1>" ++
+        "</body></html>");
+    var stream_writer = conn.stream.writer(&.{});
+    const writer = &stream_writer.interface;
+    _ = try writer.write(message);
 }
