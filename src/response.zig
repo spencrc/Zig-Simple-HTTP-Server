13cb61d29f40539deefae4614c13156aceda64e2
spencrc@debian <145073726+spencrc@users.noreply.github.com>
2025-10-14 17:57:56 -0400

    created HEADER_TEMPLATE in module space; Response.write is now easier to read

--- src/response.zig
+++ src/response.zig
@@ -3,6 +3,12 @@ const Connection = std.net.Server.Connection;
 
 const Writer = std.Io.Writer;
 
+const HEADER_TEMPLATE = //please see write function for what gets placed in formatting specifiers
+    "HTTP/1.1 {d} \r\n" ++
+    "Content-Length: {d} \r\n" ++
+    "Content-Type: text/html \r\n" ++
+    "Connection: close \r\n\r\n";
+
 pub const Response = struct {
     conn: Connection,
     status: u16,
@@ -18,8 +24,8 @@ pub const Response = struct {
 
     pub fn write(self: Response) !void {
         var header_buffer: [220]u8 = undefined;
-        // HTTP/1.1 {d} \r\n is ALWAYS 15 characters, as {d} is ALWAYS a 3-digit number
-        const headers = try std.fmt.bufPrint(header_buffer[0..], "HTTP/1.1 {d} \r\nContent-Length: {d} \r\nContent-Type: text/html \r\nConnection: close \r\n\r\n", .{
+
+        const headers = try std.fmt.bufPrint(header_buffer[0..], HEADER_TEMPLATE, .{
             self.status,
             self.body.len,
         });
@@ -27,6 +33,5 @@ pub const Response = struct {
         var stream_writer = self.conn.stream.writer(&.{});
         const writer = &stream_writer.interface;
         _ = try writer.writeVec(&.{ headers, self.body });
-        //_ = try writer.write(self.body);
     }
 };
